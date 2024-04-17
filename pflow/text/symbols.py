7500bd5dd8c75b68cc0577034401334afe85b246
hans <ohs2251@naver.com>
2024-04-17 14:03:42 +0900

    for training korean dataset and concat audio files

--- pflow/text/symbols.py
+++ pflow/text/symbols.py
@@ -1,17 +1,25 @@
-""" from https://github.com/keithito/tacotron
+""" from https://github.com/keithito/tacotron """
 
+"""
 Defines the set of symbols used in text input to the model.
 """
-_pad = "_"
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_letters_ipa = (
-    "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-)
+# _pad = "_"
+# _punctuation = ';:,.!?¡¿—…"«»“” '
+# _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+# _letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
 
 
-# Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+# # Export all symbols:
+# symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
 
-# Special symbol ids
-SPACE_ID = symbols.index(" ")
+# # Special symbol ids
+# SPACE_ID = symbols.index(" ")
+
+
+
+#import cmudict
+#from korean import KOR_SYMBOLS
+from .korean import KOR_SYMBOLS
+
+kor_symbols=KOR_SYMBOLS
+symbols=kor_symbols
